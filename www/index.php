30022b54cf77e423d45ad5c700dedfcfd6de5a26
alekslesik <alekslesik@gmail.com>
2023-05-23 15:37:34 +0300

    open www

---
+++ www/index.php
@@ -0,0 +1,7 @@
+
+
+<h4><a href="https://github.com/alekslesik/bitrix-docker" style="font-family: Verdana,Arial, Helvetica, sans-serif; color:crimson">https://github.com/alekslesik/bitrix-docker</a></h4>
+
+
+<?php include "./bitrix_server_test.php"  ?>
+
